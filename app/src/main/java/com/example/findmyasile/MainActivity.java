8ad77bf2e522ee5f93670700f9caf5e8ea20ac9a
nishchayjeet <nishchayjeets@gmail.com>
2020-03-31 17:40:49 -0700

    Splash Screen Removeed fOR soMETIME

--- app/src/main/java/com/example/findmyasile/MainActivity.java
+++ app/src/main/java/com/example/findmyasile/MainActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Intent intent;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
 
     }
 
